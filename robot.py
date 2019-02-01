cb2f1f5cd3c1d36d379001b811f513f335e06e93
Brett Tomita <bretttomita@gmail.com>
2019-01-31 21:43:34 -0800

    Each run should completely clear the top left 3x3 square

--- robot.py
+++ robot.py
@@ -7,32 +7,117 @@ class Robot:
         self.pos = (0, 0)
         self.track = [self.pos]
 
-    def scan(self):
+    def setZero(self, home):    # Set map value of self.pos to 0
+        home[self.pos[0]][self.pos[1]] = 0
 
-        movingRight = True
+    def scan(self, home):
+
+        movingRight, movingLeft = False, False
+
+        if self.pos == (0,0):
+            if home[0][0] == 1:
+                self.setZero(home)
+            self.downRight(home)
+            movingRight = True
+            movingLeft = False
+        else:
+            movingLeft = True
+            movingRight = False
 
         if movingRight:
-            up = (self.pos[0]-1, self.pos[1])
-            right = (self.pos[0], self.pos[1] + 1)
-            upRight = (self.pos[0]-1, self.pos[1]+1)
+            # Get values of surrounding area
+            up = home[self.pos[0]-1][self.pos[1]]
+            down = home[self.pos[0]+1][self.pos[1]]
+            right = home[self.pos[0]][self.pos[1] + 1]
+            left = home[self.pos[0]][self.pos[1]-1]
+            upLeft = home[self.pos[0]-1][self.pos[1]-1]
+            upRight = home[self.pos[0]-1][self.pos[1]+1]
+            downLeft = home[self.pos[0]+1][self.pos[1]-1]
+            downRight = home[self.pos[0]+1][self.pos[1]+1]
 
             if up == 1:
-                if right == 1 and upRight == 0:
-                    self.up
-                    self.downLeft
-                    self.down
-                    self.right
-                    self.right
-                    self.up
-                else:   # All three = 1
-                    self.up
-                    self.downLeft
-                    self.down
-                    self.right
-                    self.right
-                    self.up
-                    self.up
+                if right == 1:
+                    self.up(home)
+                    self.downLeft(home)
+                    self.down(home)
+                    self.right(home)
+                    if downRight == 1:
+                        self.right(home)
+                        self.up(home)
+                    else:
+                        self.upRight(home)
+                    if upRight == 1:
+                        self.up(home)
+                elif upRight == 1:  # right = 0 
+                    self.upRight(home)
+                    self.left(home)
+                    self.downLeft(home)
+                    self.down(home)
+                    self.right(home)
+                    self.right(home)
+                else:   # right & upRight = 0
+                    self.up(home)
+                    self.downLeft(home)
+                    self.down(home)
+                    self.right(home)
+                    self.right(home)
 
+            elif left == 1: # up = 0
+                self.left(home)
+                if downLeft == 1:
+                    self.down(home)
+                    self.right(home)
+                else:   # up = 0, downLeft = 0, left = 1
+                    self.downRight(home)
+                if downRight == 1:
+                    self.right(home)
+                    if upRight == 1:
+                        self.up(home)
+                        self.up(home)
+                    elif right == 1:
+                        self.up(home)
+                else:   # downright = 0
+                    self.upRight(home)
+                    if upRight == 1:
+                        self.up(home)
+                #if upRight == 1:
+                 #   self.up(home)
+            elif downLeft == 1: # up = 0, left = 0
+                self.downLeft(home)
+                self.right(home)
+                if downRight == 1:
+                    self.right(home)
+                    if upRight == 1:
+                        self.up(home)
+                        self.up(home)
+                    elif right == 1:
+                        self.up(home)
+                else:
+                    self.upRight(home)
+                if upRight == 1:
+                    self.up(home)
+
+            elif down == 1: # up = 0, left = 0, downLeft = 0
+                self.down(home)
+                if downRight == 1:
+                    self.right(home)
+                    if upRight == 1:
+                        self.up(home)
+                        self.up(home)
+                    elif right == 1:
+                        self.up(home)
+                else:
+                    self.upRight(home)
+                    if upRight == 1:
+                        self.up(home)
+
+            else:   # up = 0, left = 0, down = 0, downLeft = 0
+                self.downRight(home)
+                if upRight == 1:
+                    self.up(home)
+                    self.up(home)
+                elif right == 1:
+                    self.up(home)
             if right == 1:
                 pass
 
@@ -43,57 +128,66 @@ class Robot:
             pass
 
 
-    def up(self):
+    def up(self, home):
         if self.pos[0] > 0:    # If not on the top row, move up one row
             self.pos = (self.pos[0]-1, self.pos[1]) # Decrease row by 1
+            self.setZero(home)
             self.track.append(self.pos) # Add new position to track
 
 
-    def down(self):
+    def down(self, home):
         if self.pos[0] < 18:    # If not on the bottom row, move down one row
             self.pos = (self.pos[0]+1, self.pos[1]) # Increase row by 1
+            self.setZero(home)
             self.track.append(self.pos) # Add new position to track
 
 
-    def left(self):
+    def left(self, home):
         if self.pos[1] > 0:
             self.pos = (self.pos[0], self.pos[1]-1)
+            self.setZero(home)
             self.track.append(self.pos)
 
 
-    def right(self):
+    def right(self, home):
         if self.pos[1] < 18:
             self.pos = (self.pos[0], self.pos[1]+1)
+            self.setZero(home)
             self.track.append(self.pos)
 
 
-    def topLeft(self):
+    def upLeft(self, home):
         if self.pos[0] > 0 and self.pos[1] > 0:
             self.pos = (self.pos[0]-1, self.pos[1]-1)
+            self.setZero(home)
             self.track.append(self.pos)
 
 
-    def upRight(self):
+    def upRight(self, home):
         if self.pos[0] > 0 and self.pos[1] < 18:
             self.pos = (self.pos[0]-1, self.pos[1]+1)
+            self.setZero(home)
             self.track.append(self.pos)
 
 
-    def downLeft(self):
+    def downLeft(self, home):
         if self.pos[0] < 18 and self.pos[1] > 0:
             self.pos = (self.pos[0]+1, self.pos[1]-1)
+            self.setZero(home)
             self.track.append(self.pos)
 
 
-    def downRight(self):
+    def downRight(self, home):
         if self.pos[0] < 18 and self.pos[1] < 18:
             self.pos = (self.pos[0]+1, self.pos[1]+1)
+            self.setZero(home)
             self.track.append(self.pos)
 
 
     def clean(self, home):
-        matrix = np.array(home.dirty)
-        print(matrix[1][2])
+        #matrix = np.array(home.dirty)
+        #print(matrix[1][2])
+        self.scan(home)
         
         #for row in home.dirty:
             #print(row)
@@ -105,7 +199,10 @@ class Robot:
 
 if __name__ == '__main__':
     home = Map(19, 19)
+    home = np.array(home.dirty)
     agent = Robot()
+    print(home)
     agent.clean(home)
     agent.show()
-    home.show()
+    print(home)
+    #home.show()
