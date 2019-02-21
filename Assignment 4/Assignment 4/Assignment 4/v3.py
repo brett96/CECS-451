baaefddf516a1dbce745a65c753fe6a913ebaa3d
Alan Nguyen <alan.csulb@gmail.com>
2019-02-21 13:53:39 -0800

    Update v3.py
    fixed negative fitness values

--- Assignment 4/Assignment 4/Assignment 4/v3.py
+++ Assignment 4/Assignment 4/Assignment 4/v3.py
@@ -6,7 +6,7 @@ import math
 n = int(sys.argv[1])
 k = int(sys.argv[2])
 MAX_FITNESS_SCORE = math.factorial(n) / math.factorial(2) / math.factorial(n-2)
-MAX_ITERATION = 1000
+MAX_ITERATION = 100
 MUTATE_PROB = 0.1
 
 class nQueens:
@@ -41,16 +41,11 @@ def makeGeneticList():
 def makePopulationList(populationSize):
     population = [nQueens() for i in range(populationSize)]
     for i in range(populationSize):
-        
-        
         population[i].setBoard(makeGeneticList())
         fitnessCheck = calculateFitness(population[i].board)
-        
-        
         while(fitnessCheck < 0):
             population[i].setBoard(makeGeneticList())
-            fitnessCheck = calculateFitness(population[i].board)    
-            
+            fitnessCheck = calculateFitness(population[i].board)      
         population[i].setFitnessScore(fitnessCheck)
         #print(population[i].board)
         #print("Population fitness: ", population[i].fitnessScore)
@@ -65,7 +60,6 @@ def calculateFitness(board):
     conflicts += rcConflicts
     #print("row and column conflicts: ", rcConflicts)
     
-    
     #calculate diagonal conflicts
     for i in range(len(board)):
         for j in range(len(board)):
@@ -77,7 +71,6 @@ def calculateFitness(board):
     
     #print("diagonal conflicts: ", conflicts - rcConflicts)
     
-    
     score = (MAX_FITNESS_SCORE - conflicts) * 1.0
     #print("conflicts: " , conflicts)
     #print("max fitness score: ", MAX_FITNESS_SCORE)
@@ -87,7 +80,6 @@ def calculateFitness(board):
 
 def roulleteWheelSelection(population):
     sumFitness = sum([i.fitnessScore for i in population])
-
     randomPick = random.uniform(0, sumFitness)
     #print("Random pick: ",randomPick)
     current = 0
@@ -110,22 +102,17 @@ def getParents(population):
     p2 = None
     
     while (p1 is None or p2 is None) and ( p1 == p2) :
+        p1 = roulleteWheelSelection(population)
+        p2 = roulleteWheelSelection(population)
         
-    #sumFitness = numpy.sum([i.fitnessScore for i in population])
-        
-    
-
-    #for p in population:
-    #    p.probability = p.fitnessScore / sumFitness
-        #print("P probability: ",p.board, "\t",  p.probability, "\t", p.fitnessScore, "/", sumFitness)
-        
-       # sumFitness = sum([i.fitnessScore for i in population])
+        #sumFitness = numpy.sum([i.fitnessScore for i in population])
+        #for p in population:
+        #    p.probability = p.fitnessScore / sumFitness
+        #print("P probability: ",p.board, "\t",  p.probability, "\t", p.fitnessScore, "/", sumFitness)   
+        # sumFitness = sum([i.fitnessScore for i in population])
         #selectionProbs = [i.fitnessScore / sumFitness for i in population]
         #p1 = population[numpy.random.choice(len(population), p = selectionProbs)]
         #p2 = population[numpy.random.choice(len(population), p = selectionProbs)]
-        
-        p1 = roulleteWheelSelection(population)
-        p2 = roulleteWheelSelection(population)
         
     if p1 is not None and p2 is not None:
         return p1, p2
@@ -160,29 +147,35 @@ def crossover(p1, p2):
     #print("Child: ", child.board, "\t",  child.fitnessScore,  "\t", child.probability)
     #print("Child: ", child.board)
     return child
-        
-
 
 def mutate(child):     
     for i in range(len(child.board)):
         if  random.random() < MUTATE_PROB:
-            print("Mutated")
-            print("Old board: ", child.board)
-            print("Old fitness score: ", child.fitnessScore)
+            #print("Mutated")
+            #print("Old board: ", child.board)
+            #print("Old fitness score: ", child.fitnessScore)
             #mutationIndex = numpy.random.randint(n-1)
             oldVal = child.board[i]
             newVal = numpy.random.randint(n-1)
             while oldVal == newVal:
                 newVal = numpy.random.randint(n-1)
             child.board[i] = newVal
-            child.setFitnessScore(calculateFitness(child.board))
             print("New board: ", child.board)
             print("New fitness score: ", child.fitnessScore)
             print("Changed: ", oldVal, " --> ", newVal, "at index: ", i, "\n")
+                newVal = numpy.random.randint(n-1)
+                while oldVal == newVal:
+                    newVal = numpy.random.randint(n-1)   
+                child.board[i] = newVal
+                fitnessCheck = calculateFitness(child.board)
+            child.setFitnessScore(fitnessCheck)
+            #child.setFitnessScore(calculateFitness(child.board))
+            #print("New board: ", child.board)
+            #print("New fitness score: ", child.fitnessScore)
+            #print("Changed: ", oldVal, " --> ", newVal, "at index: ", i, "\n")
     return child
 
 def genetic(population, iteration):
-    
     print (" #"*10 ,"Executing Genetic  generation : ", iteration + 1 , " #"*10)
     newGeneration = []
     newGeneration = [nQueens() for i in range(len(population))]
@@ -192,32 +185,25 @@ def genetic(population, iteration):
         #print("Parents: ", p1.board, "\t", p2.board)
         #print("\nParent 1: ", p1.board, "\t",  p1.fitnessScore,  "\t", p1.probability)
         #print("Parent 2: ", p2.board, "\t",  p2.fitnessScore,  "\t", p2.probability)
-        
         c = crossover(p1,p2)
-        
         c = mutate(c)
-            
         newGeneration[i].setValues(c.board, c.fitnessScore, c.probability)
     
     population.append(newGeneration)
     
     #print("New Generation: ")
     for  g in newGeneration:
-        print(g.board, "\t",  g.fitnessScore, "\t")
+        print(g.board, "\t",  g.fitnessScore)
     
     return newGeneration
                   
 def isSolved(population):
-    
     fitnessScores = [i.fitnessScore for i in population]
     #print (fitnessScores)
-    
     if MAX_FITNESS_SCORE in fitnessScores:
         return True
-    
     if iteration == MAX_ITERATION:
        return True
-    
     return False
 
 def maxFitnessScoreFound(population):
@@ -231,9 +217,9 @@ if __name__ == '__main__':
     maxScoreFound = 0
     maxScoreFounds = []
     
-    print("Original Population: ")
+    print("\nOriginal Population: ")
     for p in population:
-        print(p.board, "\t", p.fitnessScore, "\t", p.probability)
+        print(p.board, "\t", p.fitnessScore)
     
     while not isSolved(population):
  
@@ -243,8 +229,7 @@ if __name__ == '__main__':
         if(maxScoreFound < maxFitnessScoreFound(population)):
             maxScoreFound = maxFitnessScoreFound(population)
             maxScoreFounds.append(iteration)
-        
-         
+    
     print ("Iterations: ", iteration)
      
     if MAX_FITNESS_SCORE in [i.fitnessScore for i in population]:
