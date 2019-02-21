713265184399af239138598700b268a089fa5dd9
Alan-CSULB <33073904+Alan-CSULB@users.noreply.github.com>
2019-02-21 00:47:07 -0800

    Update v3.py
    fixed negative probability issue

--- Assignment 4/Assignment 4/Assignment 4/v3.py
+++ Assignment 4/Assignment 4/Assignment 4/v3.py
@@ -6,7 +6,7 @@ import math
 n = int(sys.argv[1])
 k = int(sys.argv[2])
 MAX_FITNESS_SCORE = math.factorial(n) / math.factorial(2) / math.factorial(n-2)
-MAX_ITERATION = 1000
+MAX_ITERATION = 10000
 MUTATE_PROB = 0.1
 
 
@@ -42,18 +42,29 @@ def makeGeneticList():
 def makePopulationList(populationSize):
     population = [nQueens() for i in range(populationSize)]
     for i in range(populationSize):
+        
+        
         population[i].setBoard(makeGeneticList())
+        fitnessCheck = calculateFitness(population[i].board)
+        
+        
+        while(fitnessCheck < 0):
+            population[i].setBoard(makeGeneticList())
+            fitnessCheck = calculateFitness(population[i].board)    
+            
+        population[i].setFitnessScore(fitnessCheck)
         #print(population[i].board)
-        population[i].setFitnessScore(calculateFitness(population[i].board))
         #print("Population fitness: ", population[i].fitnessScore)
     return population
         
 def calculateFitness(board):
     #number of conflicts
     conflicts = 0
-       
+    rcConflicts  = 0
     #calculate conflicts for rows && columns
-    conflicts += abs(len(board) - len(numpy.unique(board)))
+    rcConflicts += abs(len(board) - len(numpy.unique(board)))
+    conflicts += rcConflicts
+    #print("row and column conflicts: ", rcConflicts)
     
     
     #calculate diagonal conflicts
@@ -64,8 +75,15 @@ def calculateFitness(board):
                 ySlope = abs(board[i] - board[j])
                 if(xSlope == ySlope):
                     conflicts += 1
-                    
-    return MAX_FITNESS_SCORE - conflicts
+    
+    #print("diagonal conflicts: ", conflicts - rcConflicts)
+    
+    
+    score = MAX_FITNESS_SCORE - conflicts
+    #print("conflicts: " , conflicts)
+    #print("max fitness score: ", MAX_FITNESS_SCORE)
+    #print("fitness score: ", score)
+    return score
         
 # =============================================================================
 #         length = len(list)
@@ -89,26 +107,37 @@ def calculateFitness(board):
 # 
 # =============================================================================
 
+def roulleteWheelSelection(population):
+    sumFitness = sum([i.fitnessScore for i in population])
+    randomPick = random.uniform(0, sumFitness)
+    current = 0
+    for p in population:
+        current += p.fitnessScore
+        if current > randomPick:
+            return p
 
 
-
-def getParents():
+def getParents(population):
     p1 = None
     p2 = None
     
     while p1 != p2 or p1 is None or p2 is None:
-    
+        
     #sumFitness = numpy.sum([i.fitnessScore for i in population])
-        sumFitness = sum([i.fitnessScore for i in population])
+        
+    
 
     #for p in population:
     #    p.probability = p.fitnessScore / sumFitness
         #print("P probability: ",p.board, "\t",  p.probability, "\t", p.fitnessScore, "/", sumFitness)
         
+        sumFitness = sum([i.fitnessScore for i in population])
         selectionProbs = [i.fitnessScore / sumFitness for i in population]
-    
         p1 = population[numpy.random.choice(len(population), p = selectionProbs)]
         p2 = population[numpy.random.choice(len(population), p = selectionProbs)]
+        
+        #p1 = roulleteWheelSelection(population)
+        #p2 = roulleteWheelSelection(population)
         
     if p1 is not None and p2 is not None:
         return p1, p2
@@ -197,9 +226,21 @@ def crossover(p1, p2):
     
     child.setBoard(cBoard)
     
+    fitnessCheck = calculateFitness(child.board)
     #sumFitness = numpy.sum([i.fitnessScore for i in population])
-    child.setFitnessScore(calculateFitness(child.board))
+    
+    while(fitnessCheck < 0):
+        p1, p2 = getParents(population)
+        cBoard = []
+        cBoard.extend(p1.board[0:crossoverIndex])
+        cBoard.extend(p2.board[crossoverIndex:])  
+        child.setBoard(cBoard)
+        fitnessCheck = calculateFitness(child.board)
+
+        
+    child.setFitnessScore(fitnessCheck)
     #child.setProbability(child.fitnessScore / sumFitness)
+
     
     #print("\nChild: ", child.board, "\t",  child.fitnessScore,  "\t", child.probability, "\n")
     return child
@@ -225,7 +266,7 @@ def mutate(child):
             #print("Mutated")
             #print("Old board: ", child.board)
             #mutationIndex = numpy.random.randint(n-1)
-            oldVal = child.board[i]
+            #oldVal = child.board[i]
             newVal = numpy.random.randint(n-1)
             child.board[i] = newVal
             #print("New board: ", child.board)
@@ -239,7 +280,7 @@ def genetic(iteration):
     newGeneration = [nQueens() for i in range(populationSize)]
         
     for i in range(len(population)):
-        p1, p2 = getParents()
+        p1, p2 = getParents(population)
         #print("\nParent 1: ", p1.board, "\t",  p1.fitnessScore,  "\t", p1.probability)
         #print("Parent 2: ", p2.board, "\t",  p2.fitnessScore,  "\t", p2.probability)
         
@@ -249,9 +290,9 @@ def genetic(iteration):
     
     population.append(newGeneration)
     
-    print("New Generation: ")
-    for  g in newGeneration:
-      print(g.board, "\t",  g.fitnessScore, "\t",  g.probability)
+    #print("New Generation: ")
+    #for  g in newGeneration:
+    #  print(g.board, "\t",  g.fitnessScore, "\t",  g.probability)
     
     return newGeneration
                   
@@ -312,11 +353,16 @@ def isSolved():
 #         print(each.board)
 # =============================================================================
 
+def maxFitnessScoreFound(population):
+    fitnessScores = [i.fitnessScore for i in population]
+    return max(fitnessScores)    
+
 if __name__ == '__main__':
     iteration = 0
     populationSize = k
     population = makePopulationList(populationSize)
- 
+    maxScoreFound = 0
+    maxScoreFounds = []
     #print("Original Population: ")
     #for p in population:
     #    print(p.board, "\t", p.fitnessScore, "\t", p.probability)
@@ -325,13 +371,24 @@ if __name__ == '__main__':
  
         population = genetic(iteration)
         iteration += 1
+        
+        if(maxScoreFound < maxFitnessScoreFound(population)):
+            maxScoreFound = maxFitnessScoreFound(population)
+            maxScoreFounds.append(iteration)
+        
          
-    print ("Iteration: ", iteration)
+    print ("Iterations: ", iteration)
      
-    print ("Successful boards: ") 
-    for each in population:
-        if each.fitnessScore == MAX_FITNESS_SCORE :
-            print(each.board)
+    if MAX_FITNESS_SCORE in [i.fitnessScore for i in population]:
+        print ("Successful boards: ") 
+        for each in population:
+            if each.fitnessScore == MAX_FITNESS_SCORE :
+                print(each.board)
+    else:
+        print("Max fitness score found: ", maxScoreFound)
+        print("Max fitness score lists amount: ",  len(maxScoreFounds))
+        print("Max fitness score found list: ", maxScoreFounds)
+
 
 
 # =============================================================================
@@ -363,8 +420,3 @@ if __name__ == '__main__':
 #         print("Parent 1 = ", controller.p1)
 #         print("Parent 2 = ", controller.p2)
 # =============================================================================
-
-
-
-
-
