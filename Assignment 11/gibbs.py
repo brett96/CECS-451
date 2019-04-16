36fb98dcb9f7e4f30aa1bcdacd071d8e3bf1d18a
Brett Tomita <bretttomita@gmail.com>
2019-04-16 10:51:50 -0700

    Assignment 11 initial commit

---
+++ Assignment 11/gibbs.py
@@ -0,0 +1,14 @@
+def gibbs(numSamples, x, e):
+	counts = []
+	nonEvidence = []
+	currentState = e
+
+	for j in range(1, counts):
+		for z in nonEvidence:
+			#set the value of Zi in x by sampling from P(Zi|mb(Zi))
+			N[x]←N[x] + 1 where x is the value of X in x
+			counts[x] += 1
+	return Normalize(N)
+
+def Normalize(N):
+	pass
