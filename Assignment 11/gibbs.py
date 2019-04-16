c978c855eae01de631f2fd1ec4a23b49be7a3150
Brett Tomita <bretttomita@gmail.com>
2019-04-16 11:05:39 -0700

    Assignment 11 initial commit

--- Assignment 11/gibbs.py
+++ Assignment 11/gibbs.py
@@ -1,12 +1,12 @@
 def gibbs(numSamples, x, e):
-	counts = []
-	nonEvidence = []
-	currentState = e
+	counts = [0, 0, 0, 0]
+	nonEvidence = ["cloudy", "rain"]
+	currentState = [True, True, False, False]
+	probCloudy = 0.5
 
 	for j in range(1, counts):
 		for z in nonEvidence:
 			#set the value of Zi in x by sampling from P(Zi|mb(Zi))
-			N[x]←N[x] + 1 where x is the value of X in x
 			counts[x] += 1
 	return Normalize(N)
 
