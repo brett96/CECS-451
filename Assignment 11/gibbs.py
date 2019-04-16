0fc4af76c7765fc091c5b1ed48edce515747bcf8
Alan Nguyen <alan.csulb@gmail.com>
2019-04-16 11:59:22 -0700

    Update gibbs.py
    created normalize method

--- Assignment 11/gibbs.py
+++ Assignment 11/gibbs.py
@@ -2,7 +2,16 @@ def gibbs(numSamples, x, e):
 	counts = [0, 0, 0, 0]
 	nonEvidence = ["cloudy", "rain"]
 	currentState = [True, True, False, False]
-	probCloudy = 0.5
+	probCloudy = [0.5]
+	probSprinkler = [0.1, 0.5]
+	probRain = [0.8, 0.2]
+	probWetGrass = [0.99, 0.9, 0.9, 0]
+
+
+	#P(C|S,R)
+	#P(C|S,~R)
+	#P(R|C,S,W)
+	#P(R|~C,S,W)
 
 	for j in range(1, counts):
 		for z in nonEvidence:
@@ -11,4 +20,5 @@ def gibbs(numSamples, x, e):
 	return Normalize(N)
 
 def Normalize(N):
+	norm = [float(i)/max(N) for i in N]
 	pass
